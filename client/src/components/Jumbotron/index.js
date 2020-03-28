85c2746e8fee735618d836dfe8cdd594075a1cb0
Alex Dollard <alexdollard141@gmail.com>
2020-03-28 11:03:24 -0500

    starting to import google Map

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -2,9 +2,10 @@ import React from "react";
 
 function Jumbotron({ children }) {
   return (
+    <h3>Google Maps Demo</h3>
     <div
       style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-      className="jumbotron"
+      className="jumbotron" id="map"
     >
       {children}
     </div>
