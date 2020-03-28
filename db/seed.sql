72f5f936031087cc33f162bda70d937868becfbe
Jamie <kookjc6@gmail.com>
2020-03-28 10:57:33 -0500

    added connection to sql database and passport authentication files

---
+++ db/seed.sql
@@ -0,0 +1,18 @@
+-- USE ice_creamDB;
+
+-- CREATE TABLE products (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   flavor VARCHAR(45) NULL,
+--   price DECIMAL(10,2) NULL,
+--   quantity INT NULL,
+--   PRIMARY KEY (id)
+-- );
+
+-- INSERT INTO products (flavor, price, quantity)
+-- VALUES ("vanilla", 2.50, 100);
+
+-- INSERT INTO products (flavor, price, quantity)
+-- VALUES ("chocolate", 3.10, 120);
+
+-- INSERT INTO products (flavor, price, quantity)
+-- VALUES ("strawberry", 3.25, 75);
