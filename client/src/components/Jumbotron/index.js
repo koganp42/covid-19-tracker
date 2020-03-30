38e5aaf8cf79e2747e2819af377fd57bdefe3cb9
Alex Dollard <alexdollard141@gmail.com>
2020-03-30 12:17:57 -0500

    push to allow checkout

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function Jumbotron({ children }) {
   return (
-    <h3>Google Maps Demo</h3>
     <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-      className="jumbotron" id="map"
+    style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
+    className="jumbotron"
     >
+    <h3>Google Maps Demo</h3>
       {children}
     </div>
   );
