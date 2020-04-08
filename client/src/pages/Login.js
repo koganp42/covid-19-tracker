d91c1efdf2c0f3369c410e072a24bccaf988d373
Jamie <kookjc6@gmail.com>
2020-04-08 15:26:24 -0600

    added userState into login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles , Grid} from '@material-ui/core/styles';
 import { Button } from "@material-ui/core"
 import TextField from '@material-ui/core/TextField';
@@ -14,12 +14,37 @@ const useStyles = makeStyles((theme) => ({
 
 export default function BasicTextFields() {
   const classes = useStyles();
+  const [userInfo, setUserInfo]= useState({email: "", password: ""});  
+
+  //   const handleInputChange= ()=>{
+//     dispatch({type: "handleSearchInput", "searchInput": searchInput.current.value}); 
+//   }
+
+  const handleChange= (event)=>{
+    const name= event.target.name; 
+    const value = event.target.value; 
+    console.log(value); 
+    setUserInfo({...userInfo, [name]: value}); 
+    console.log(userInfo); 
+  }
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
         <h2>Login</h2>
-      <TextField id="standard-basic" label="email" />
-      <TextField id="standard-basic" label="password" />
+      <TextField 
+        value= {userInfo.email} 
+        name="email"
+        onChange={handleChange}
+        id="standard-basic" 
+        label="email" 
+      />
+      <TextField 
+        value= {userInfo.password} 
+        name="password"
+        onChange={handleChange}
+        id="standard-basic" 
+        label="password" 
+      />
       <Button variant="contained">Enter</Button>
     </form>
   );
