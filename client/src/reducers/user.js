976d0f46715ec3e3004d0a6297002f1403af2a22
Alex Dollard <alexdollard141@gmail.com>
2020-04-09 18:31:37 -0500

    created basic User reducer, and createUser action

---
+++ client/src/reducers/user.js
@@ -0,0 +1,29 @@
+import { CREATE_USER } from '../actions/types'
+
+//makeAPI call to get all users from db, and use that as initial state?
+const initialState = {
+    firstName: "",
+    lastName: "",
+    age: 0,
+    dateOfBirth: new Date(),
+    sex: "female",
+    lat: 0,
+    long: 0,
+    smoking: "never",
+    preExistingConditions: "false",
+    listPreExistingConditions: "",
+    sick: "false",
+    userID: 0
+}; 
+
+export default function(state = initialState, action){
+    switch(action.type) {
+        case CREATE_USER:
+            return [...state, action.payload]
+        default:
+        return state
+
+    }
+
+
+}
