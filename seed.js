0ff31fcff180143547b796455e1eb9ccff1bab2a
Alex Dollard <alexdollard141@gmail.com>
2020-04-14 20:30:46 -0500

    many more seed entries

--- seed.js
+++ seed.js
@@ -13,13 +13,63 @@ const usersToSeed = [
         email: "brenda@work.com",
         password:  "1234pass",
         admin: "false"
+    },
+    {
+        email: "marshall@work.com",
+        password:  "1234pass",
+        admin: "false"
+    },
+    {
+        email: "mojo@work.com",
+        password:  "1234pass",
+        admin: "false"
+    },
+    {
+        email: "carmine@work.com",
+        password:  "1234pass",
+        admin: "false"
+    },
+    {
+        email: "jenine@work.com",
+        password:  "1234pass",
+        admin: "false"
+    },
+    {
+        email: "tilda@work.com",
+        password:  "1234pass",
+        admin: "false"
+    },
+    {
+        email: "aña@work.com",
+        password:  "1234pass",
+        admin: "false"
+    },
+    {
+        email: "ryan@work.com",
+        password:  "1234pass",
+        admin: "false"
+    },
+    {
+        email: "carl@work.com",
+        password:  "1234pass",
+        admin: "false"
+    },
+    {
+        email: "timothy@work.com",
+        password:  "1234pass",
+        admin: "false"
+    },
+    {
+        email: "henry@work.com",
+        password:  "1234pass",
+        admin: "false"
     }
 ]
 
 const peopleToSeed= [
     {
         firstName: "Ben",
-        lastName:"Simmons",
+        lastName:"Hale",
         age: 44,
         sex:"male",
         lat: 36.151490,
@@ -38,8 +88,128 @@ const peopleToSeed= [
         lon: -86.801790,
         smoking: "never",
         preExistingConditions: "false",
-        sick: "false",
+        sick: "true",
         UserId: 2 
+    },    
+    {
+        firstName: "Marshall",
+        lastName:"Claiborne",
+        age: 25,
+        sex:"male",
+        lat: 35.938750,
+        lon: -87.601790,
+        smoking: "never",
+        preExistingConditions: "false",
+        sick: "false",
+        UserId: 3 
+    },    
+    {
+        firstName: "Mojo",
+        lastName:"Jojo",
+        age: 90,
+        sex:"male",
+        lat: 35.638750,
+        lon: -86.901790,
+        smoking: "never",
+        preExistingConditions: "true",
+        sick: "true",
+        UserId: 4 
+    },    
+    {
+        firstName: "Carmine",
+        lastName:"Bosc",
+        age: 60,
+        sex:"male",
+        lat: 36.368750,
+        lon: -87.021790,
+        smoking: "never",
+        preExistingConditions: "true",
+        sick: "true",
+        UserId: 5 
+    },    
+    {
+        firstName: "Jennine",
+        lastName:"Peck",
+        age: 8,
+        sex:"female",
+        lat: 36.298750,
+        lon: -87.028790,
+        smoking: "never",
+        preExistingConditions: "false",
+        sick: "true",
+        UserId: 6 
+    },    
+    {
+        firstName: "Tilda",
+        lastName:"Fuentes",
+        age: 82,
+        sex:"female",
+        lat: 35.714750,
+        lon: -86.640790,
+        smoking: "never",
+        preExistingConditions: "true",
+        sick: "false",
+        UserId: 7 
+    },    
+    {
+        firstName: "Aña",
+        lastName:"Machado",
+        age: 30,
+        sex:"female",
+        lat: 36.248750,
+        lon: -86.701790,
+        smoking: "never",
+        preExistingConditions: "false",
+        sick: "true",
+        UserId: 8 
+    },    
+    {
+        firstName: "Ryan",
+        lastName:"Allen-Grave",
+        age: 56,
+        sex:"male",
+        lat: 34.988750,
+        lon: -86.8901790,
+        smoking: "currently",
+        preExistingConditions: "true",
+        sick: "true",
+        UserId: 9 
+    },    
+    {
+        firstName: "Carl",
+        lastName:"Dietrich",
+        age: 17,
+        sex:"male",
+        lat: 36.278750,
+        lon: -86.491790,
+        smoking: "false",
+        preExistingConditions: "false",
+        sick: "false",
+        UserId: 10 
+    },    
+    {
+        firstName: "Timothy",
+        lastName:"Voss",
+        age: 73,
+        sex:"male",
+        lat: 36.6038750,
+        lon: -86.501790,
+        smoking: "never",
+        preExistingConditions: "false",
+        sick: "true",
+        UserId: 11 
+    },    
+    {
+        firstName: "Henry",
+        lastName:"Leake",
+        age: 48,
+        sex:"male",
+        lat: 36.618750,
+        lon: -86.251790,
+        smoking: "never",
+        preExistingConditions: "false",
+        sick: "true",
+        UserId: 12 
     }
 ]
 
@@ -66,7 +236,7 @@ const illnessesToSeed= [
         "intensiveCare": "true",
         "death": "true",
         "dateOfRecovery": "2020-03-17",
-        "PersonId": 1
+        "PersonId": 2
     },
     {
         "tested": "true",
@@ -78,7 +248,7 @@ const illnessesToSeed= [
         "intensiveCare": "true",
         "death": "false",
         "dateOfRecovery": "2020-03-17",
-        "PersonId": 1
+        "PersonId": 4
     },
     {
         "tested": "true",
@@ -88,9 +258,9 @@ const illnessesToSeed= [
         "hospitalized": "true",
         "dateOfHospitalization": "2020-03-10",
         "intensiveCare": "true",
-        "death": "true",
+        "death": "false",
         "dateOfRecovery": "2020-03-10",
-        "PersonId": 1
+        "PersonId": 5
     },
     {
         "tested": "true",
@@ -102,7 +272,7 @@ const illnessesToSeed= [
         "intensiveCare": "true",
         "death": "false",
         "dateOfRecovery": "2020-03-17",
-        "PersonId": 1
+        "PersonId": 6
     },
     {
         "tested": "true",
@@ -114,7 +284,7 @@ const illnessesToSeed= [
         "intensiveCare": "false",
         "death": "false",
         "dateOfRecovery": "2020-03-09",
-        "PersonId": 1
+        "PersonId": 8
     },
     {
         "tested": "true",
@@ -126,7 +296,31 @@ const illnessesToSeed= [
         "intensiveCare": "false",
         "death": "false",
         "dateOfRecovery": "2020-04-08",
-        "PersonId": 1
+        "PersonId": 9
+    },
+    {
+        "tested": "true",
+        "dateOfTest": "2020-03-10",
+        "dateOfOnset": "2020-03-26",
+        "symptoms": "dry cough and fever",
+        "hospitalized": "true",
+        "dateOfHospitalization": "2020-03-29",
+        "intensiveCare": "false",
+        "death": "false",
+        "dateOfRecovery": "2020-04-08",
+        "PersonId": 11
+    },
+    {
+        "tested": "true",
+        "dateOfTest": "2020-03-10",
+        "dateOfOnset": "2020-03-26",
+        "symptoms": "dry cough and fever",
+        "hospitalized": "true",
+        "dateOfHospitalization": "2020-03-29",
+        "intensiveCare": "false",
+        "death": "true",
+        "dateOfRecovery": "2020-04-08",
+        "PersonId": 12
     }
 ]
 
