45f685bb51dc2e6e24c169da9d3fa4e11a08fbfd
Alex Dollard <alexdollard141@gmail.com>
2020-04-16 20:54:44 -0500

    icon changes depending on person state / googleAPI key as env. variable

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -4,15 +4,25 @@ import patientData from "../../utils/patientData";
 import mapStyles from "../../styleStuff/mapStyles"
 import API from "../../utils/API"
 import "./style.css"
+require('dotenv').config();
 
 
 function Map(props) {
 
     const [selectedPin, updateSelectedPin] = useState(null);
     const [people, setPeople] = useState([]);
+    const [admin, setAdmin]= useState(false); 
 
   useEffect(() => {
     loadPeople()
+    API.checkAuthentication()
+        .then(user=>{
+            if (user.data.admin==="true"){
+                console.log("setting admin"); 
+                setAdmin(true); 
+            }
+        })
+        .catch(err=>console.log(err)); 
   }, [])
 
   // Loads all People and sets them to People
@@ -41,8 +51,8 @@ function Map(props) {
              
 
             {people.map(patient => (
-                
-                <Marker 
+                patient.sick==='true' ? 
+                (<Marker 
                 key={patient.id} 
                 position={{lat:patient.lat, lng:patient.lon}} 
 
@@ -50,33 +60,51 @@ function Map(props) {
                     updateSelectedPin(patient)
                 }}
                 icon={{
-                    url: patient.sick ? "/coronavirus.png": "/pharmacy.png",
+                    url: "/coronavirus.png",
                     scaledSize: new window.google.maps.Size(25, 25)
                 }}
-                /> 
+                />) : 
+                (<Marker 
+                key={patient.id} 
+                position={{lat:patient.lat, lng:patient.lon}} 
+
+                onClick={() => {
+                    updateSelectedPin(patient)
+                }}
+                icon={{
+                    url: "/pharmacy.png",
+                    scaledSize: new window.google.maps.Size(25, 25)
+                }}
+                />)
+
                 ))}
 
             {selectedPin && (
+                 (admin ? (
                 <InfoWindow 
-                position={{
+                    position={{
 
-                    lat:selectedPin.lat, lng:selectedPin.lon
+                        lat:selectedPin.lat, lng:selectedPin.lon
 
-                }} 
-                onCloseClick={() => {
-                    updateSelectedPin(null)
-                }}
-                >
-                    <div>
-                        <h5>ID: {selectedPin.id}</h5>
+                    }} 
+                    onCloseClick={() => {
+                        updateSelectedPin(null)
+                    }}
+                    >
+                        <div>
+                            <h5>ID: {selectedPin.id}</h5>
 
-                        <p>Name: {`${selectedPin.firstName} ${selectedPin.lastName}`}</p>
+                            <p>Name: {`${selectedPin.firstName} ${selectedPin.lastName}`}</p>
 
-                        <p>Age: {selectedPin.age}</p>
-                        <p>Sex: {selectedPin.sex}</p>
-                        <p>Smoker: {selectedPin.smoker===true ? "Yes" : "No"}</p>
-                    </div>
+                            <p>Age: {selectedPin.age}</p>
+                            <p>Sex: {selectedPin.sex}</p>
+                            <p>Smoker: {selectedPin.smoker===true ? "Yes" : "No"}</p>
+                        </div>
                 </InfoWindow>
+                 ) : (
+                <div></div>
+                 )
+                )
             )}
 
         </GoogleMap>
@@ -89,16 +117,15 @@ function Map(props) {
 const WrappedMap = withScriptjs(withGoogleMap(Map))
 
 export default function App(props) {
-    // let API_KEY = {AIzaSyACzcnTqh0EIE1HAJ1E605RBcwlWHAQ0Mw}
+
     
     
     return(
-        
        <Fragment >
         <div  className={"mainDiv"}>
-
+            {/* {process.env.API_KEY} */}
             <WrappedMap 
-                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyACzcnTqh0EIE1HAJ1E605RBcwlWHAQ0Mw`}
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_API_KEY}`}
                 loadingElement={<div style={{height:'100%'}} className={"mapContainer"}/>}
                 containerElement={<div style={{height:'100%', width: props.graphSelect.mapWidth}} className={"mapContainer"} />}
                 mapElement={<div style={{height:'100%'}} className={"mapContainer"}/>}
@@ -110,9 +137,5 @@ export default function App(props) {
 
 
         </Fragment>
-
-   
-
-   
     )
 };
